ef5a54a9dc5d070900682e2c0ccb7dc525b03bfc
unknown <rurbonas@spartaglobal.com>
2021-06-30 16:12:22 +0100

    first working code

--- app/exchange_rate_paser.py
+++ app/exchange_rate_paser.py
@@ -1,8 +1,12 @@
 import json
 class ExchangeRates:
-    def parse(self, xfile):
+    def parse(self, xfile, country):
         try:
-            with open(xfile, "r") as content:
-                return json.loads(content.read())
-        except SyntaxError as err:
-            return "Wrong file"
+            with open(xfile, "r") as content: # open file into contents variable
+                exchange = json.load(content) # JSON object as dictionary
+                rates = exchange["rates"][country]
+                return rates
+        except FileNotFoundError as err:
+            return "File not found"
+        except KeyError as err:
+            return "Bad country code"
--- program.py
+++ program.py
@@ -2,4 +2,8 @@ from app.exchange_rate_paser import ExchangeRates
 
 parse = ExchangeRates()
 
-xfile = excha # input("Name of the file you would like to open? >> ")
+
+country = input("What country code would you like to see >> ").upper()
+xfile = "exchange_rates.json" # input("Name of the file you would like to open? >> ")
+
+print(parse.parse(xfile, country))
